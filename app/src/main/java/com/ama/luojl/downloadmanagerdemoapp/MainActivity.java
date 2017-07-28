be772fb50cfafcb83f4cc11e2371426ffd4cb633
Jun Luo <luojdev@gmail.com>
2017-07-29 01:10:42 +0800

    Add TextView, Button, Query
    TextView to show info
    Button: cancel all
    Query onResue

--- app/src/main/java/com/ama/luojl/downloadmanagerdemoapp/MainActivity.java
+++ app/src/main/java/com/ama/luojl/downloadmanagerdemoapp/MainActivity.java
@@ -5,16 +5,20 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.FileNotFoundException;
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
     private DownloadReceiver mDownloadReceiver;
 
     private Button mDownloadBtn;
+    private Button mCancelAllBtn;
     private TextView mPromptTextView;
+
+    private HashSet<Long> mEnqueuedIDSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,25 +44,52 @@ public class MainActivity extends AppCompatActivity {
 
         mDM = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
 
-        mDownloadBtn = (Button) findViewById(R.id.download_btn);
-        mPromptTextView = (TextView) findViewById(R.id.prompt_textView);
         initView();
+
+        mEnqueuedIDSet = new HashSet<>();
 
         mDownloadReceiver = new DownloadReceiver();
         registerReceiver(mDownloadReceiver,
                 new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        DownloadManager.Query query = new DownloadManager.Query();
+        query.setFilterByStatus(DownloadManager.STATUS_RUNNING);
+        Cursor cursor = mDM.query(query);
+        if (cursor != null) {
+            showString("running cnt: " + cursor.getCount());
+        }
+
+    }
+
     private void initView() {
+        mDownloadBtn = (Button) findViewById(R.id.download_btn);
+        mCancelAllBtn = (Button) findViewById(R.id.cancel_all_btn);
+
         mDownloadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startDownload();
             }
         });
+        mCancelAllBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelAllDownload();
+            }
+        });
+
+        mPromptTextView = (TextView) findViewById(R.id.prompt_textView);
+        mPromptTextView.setMovementMethod(new ScrollingMovementMethod());
     }
 
-
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
 
     @Override
     protected void onDestroy() {
@@ -65,8 +99,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void startDownload() {
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(testURL));
-//        request.setDestinationInExternalPublicDir(DOWNLOAD_FOLDER_NAME, DOWNLOAD_FILE_NAME);
-        mDM.enqueue(request);
+        long id = mDM.enqueue(request);
+        mEnqueuedIDSet.add(id);
+        showString("ID: " + id + ", enqueued");
+    }
+
+    private void cancelAllDownload() {
+        for (long id : mEnqueuedIDSet) {
+            mDM.remove(id);
+            showString("ID: " + id + ", removed");
+        }
+        mEnqueuedIDSet.clear();
     }
 
     private class DownloadReceiver extends BroadcastReceiver {
@@ -76,17 +119,20 @@ public class MainActivity extends AppCompatActivity {
             long downloadId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
             String action = intent.getAction();
             if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
-                Log.i(TAG, "Download Complete for ID: " + downloadId);
-                ParcelFileDescriptor fileDescriptor;
-                try {
-                    fileDescriptor = mDM.openDownloadedFile(downloadId);
-                    long size = fileDescriptor.getStatSize();
-                    mPromptTextView.setText("File size: " + size);
-                } catch (FileNotFoundException e) {
-                    mPromptTextView.setText("FileNotFound");
-                }
+                showString("ID: " + downloadId + ", download complete");
+//                ParcelFileDescriptor fileDescriptor;
+//                try {
+//                    fileDescriptor = mDM.openDownloadedFile(downloadId);
+//                    long size = fileDescriptor.getStatSize();
+//                } catch (FileNotFoundException e) {
+//                    mPromptTextView.setText("FileNotFound");
+//                }
 
             }
         }
+    }
+
+    private void showString(String s) {
+        mPromptTextView.append(s + "\n");
     }
 }
